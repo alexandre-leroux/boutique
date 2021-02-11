976457d2409104b40e5d73776a2e048ce1b79a45
prénom nom <alexandre.leroux@laplateforme.io>
2021-02-11 10:44:09 +0100

    base scss ok

---
+++ models/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+class PDO{
+
+
+
+
+    public function connection_bdd()
+    {
+
+            try 
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=boutique;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            }
+            catch (Exception $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
+
+
+            return $bdd;
+
+    }
+
+
+}
