83c19df13b7db257d3d8d422d2e1b6dc7a9edfcd
prénom nom <alexandre.leroux@laplateforme.io>
2021-02-17 01:28:06 +0100

    sql à jour

--- sql/boutique (2).sql
+++ sql/boutique (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 16 fév. 2021 à 08:15
+-- Généré le : mer. 17 fév. 2021 à 00:27
 -- Version du serveur :  5.7.24
 -- Version de PHP : 7.4.1
 
@@ -30,11 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `articles` (
   `id_articles` int(11) NOT NULL,
-  `categorie_id_categorie` int(11) NOT NULL,
-  `marques_id_marques` int(11) NOT NULL,
+  `id_categorie` int(11) NOT NULL,
+  `id_marques` int(11) NOT NULL,
   `id_sous_cat_acc` int(11) DEFAULT NULL,
   `art_nom` varchar(45) NOT NULL,
-  `art_courte_desprition` varchar(255) NOT NULL,
+  `art_courte_description` varchar(255) NOT NULL,
   `art_description` longtext NOT NULL,
   `stock` int(11) NOT NULL,
   `prix` int(11) NOT NULL,
@@ -55,9 +55,9 @@ CREATE TABLE `articles` (
 -- Déchargement des données de la table `articles`
 --
 
-INSERT INTO `articles` (`id_articles`, `categorie_id_categorie`, `marques_id_marques`, `id_sous_cat_acc`, `art_nom`, `art_courte_desprition`, `art_description`, `stock`, `prix`, `raq_poids`, `raq_tamis`, `raq_taille_manche`, `raq_equilibre`, `cor_jauge`, `sac_thermobag`, `bal_conditionnement`, `bal_type`, `acc_type`, `acc_grip_eppaisseur`, `acc_grip_couleur`) VALUES
-(2, 1, 2, NULL, 'pure aero', ' ceci est une belle raquette', ' ceci est une belle raquette, je suis d\'acord', 11, 133, 300, 628, 4, 40, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(3, 1, 1, NULL, 'aero drive', ' ceci est une belle raquette jouée par nadal', ' ceci est une belle raquette, je suis d\'accord en plus elle est formidable', 8, 210, 280, 628, 3, 38, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `articles` (`id_articles`, `id_categorie`, `id_marques`, `id_sous_cat_acc`, `art_nom`, `art_courte_description`, `art_description`, `stock`, `prix`, `raq_poids`, `raq_tamis`, `raq_taille_manche`, `raq_equilibre`, `cor_jauge`, `sac_thermobag`, `bal_conditionnement`, `bal_type`, `acc_type`, `acc_grip_eppaisseur`, `acc_grip_couleur`) VALUES
+(1, 1, 1, NULL, 'pro staff', 'super raquette ', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n\r\n\r\n', 12, 150, 300, 650, 4, 30, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 1, 2, NULL, 'super court', 'super raquette top top', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n\r\n\r\n', 12, 150, 305, 650, 4, 30, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -76,10 +76,10 @@ CREATE TABLE `categorie` (
 
 INSERT INTO `categorie` (`id_categorie`, `categorie_type`) VALUES
 (1, 'raquette'),
-(2, 'balles'),
-(3, 'cordage'),
-(4, 'sacs'),
-(5, 'accessoires');
+(2, 'sacs'),
+(5, 'accessoires'),
+(6, 'balles'),
+(9, 'cordage');
 
 -- --------------------------------------------------------
 
@@ -92,6 +92,27 @@ CREATE TABLE `commandes` (
   `id_articles` int(11) NOT NULL,
   `id_utilisateurs` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `images_articles`
+--
+
+CREATE TABLE `images_articles` (
+  `id_articles` int(11) NOT NULL,
+  `chemin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `images_articles`
+--
+
+INSERT INTO `images_articles` (`id_articles`, `chemin`) VALUES
+(1, '1-1.jpg'),
+(1, '1-2.jpg'),
+(1, '1-3.jpg'),
+(1, '1-4.jpg');
 
 -- --------------------------------------------------------
 
@@ -110,8 +131,8 @@ CREATE TABLE `marques` (
 
 INSERT INTO `marques` (`id_marques`, `marques_nom`) VALUES
 (1, 'babolat'),
-(2, 'prince'),
-(3, 'head'),
+(2, 'head'),
+(3, 'prince'),
 (4, 'wilson');
 
 -- --------------------------------------------------------
@@ -152,9 +173,9 @@ CREATE TABLE `utilisateurs` (
 -- Index pour la table `articles`
 --
 ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id_articles`,`categorie_id_categorie`,`marques_id_marques`),
-  ADD KEY `fk_articles_categorie_idx` (`categorie_id_categorie`),
-  ADD KEY `fk_articles_marques1_idx` (`marques_id_marques`),
+  ADD PRIMARY KEY (`id_articles`,`id_categorie`,`id_marques`),
+  ADD KEY `fk_articles_categorie_idx` (`id_categorie`),
+  ADD KEY `fk_articles_marques1_idx` (`id_marques`),
   ADD KEY `fk_articles_sous_cat_accessoires1_idx` (`id_sous_cat_acc`);
 
 --
@@ -170,6 +191,12 @@ ALTER TABLE `commandes`
   ADD PRIMARY KEY (`id_articles`,`id_utilisateurs`,`id_commande`),
   ADD KEY `fk_articles_has_utilisateurs_utilisateurs1_idx` (`id_utilisateurs`),
   ADD KEY `fk_articles_has_utilisateurs_articles1_idx` (`id_articles`);
+
+--
+-- Index pour la table `images_articles`
+--
+ALTER TABLE `images_articles`
+  ADD KEY `id_articles` (`id_articles`) USING BTREE;
 
 --
 -- Index pour la table `marques`
@@ -197,13 +224,13 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id_articles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_articles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `marques`
@@ -231,8 +258,8 @@ ALTER TABLE `utilisateurs`
 -- Contraintes pour la table `articles`
 --
 ALTER TABLE `articles`
-  ADD CONSTRAINT `fk_articles_categorie` FOREIGN KEY (`categorie_id_categorie`) REFERENCES `categorie` (`id_categorie`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_articles_marques1` FOREIGN KEY (`marques_id_marques`) REFERENCES `marques` (`id_marques`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_articles_categorie` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_articles_marques1` FOREIGN KEY (`id_marques`) REFERENCES `marques` (`id_marques`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_articles_sous_cat_accessoires1` FOREIGN KEY (`id_sous_cat_acc`) REFERENCES `sous_cat_accessoires` (`id_sous_cat_accessoires`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
@@ -241,6 +268,12 @@ ALTER TABLE `articles`
 ALTER TABLE `commandes`
   ADD CONSTRAINT `fk_articles_has_utilisateurs_articles1` FOREIGN KEY (`id_articles`) REFERENCES `articles` (`id_articles`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_articles_has_utilisateurs_utilisateurs1` FOREIGN KEY (`id_utilisateurs`) REFERENCES `utilisateurs` (`id_utilisateurs`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `images_articles`
+--
+ALTER TABLE `images_articles`
+  ADD CONSTRAINT `fk_images_articles_articles1` FOREIGN KEY (`id_articles`) REFERENCES `articles` (`id_articles`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
