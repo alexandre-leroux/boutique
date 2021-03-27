09f5d2bc9e9a7471bdc2481159702115c7cceb49
Alexandre Leroux <alexandre.leroux@laplateforme.io>
2021-03-27 23:14:46 +0100

    page message panier ok css

--- pages/addpanier.php
+++ pages/addpanier.php
@@ -8,18 +8,41 @@ $panier = new Model_Panier();
 if(isset($_GET['id']))
 {
     $product = $panier->selectId("articles", $_GET['id']);
-    if(empty($product)){
-        die("Ce produit n'existe pas") ;
+    if(empty($product)){?>
+        <section class="section_add_panier">
+            <h1>Ce produit n'existe pas</h1></br>
+            <div>
+                <a href="javascript:history.back()">Retourner à la boutique</a> 
+            </div>
+        </section>'
+            <?php
     }
-    var_dump($product);
+    // var_dump($product);
     $panier->add($product[0]->id_articles);
-    die('le produit a bien été ajouter au panier ! <a href="javascript:history.back()">Retournez à la boutique</a> <a href="panier.php"> Voir le panier </a>'); 
+    ?>
+        <section class="section_add_panier">
+            <h1> le produit a bien été ajouter au panier ! </h1></br>
+            <div>
+                <a href="javascript:history.back()">Retourner à la boutique</a>
+            </div>
+            <div>
+                 <a href="panier.php"> Voir le panier </a>
+            </div>
+        </section>'
+            <?php
 
 }
-else{
+else{?>
+    <section class="section_add_panier">
+        <h1> Aucun produit n\'a été ajouter au panier !</h1></br>
+        <div>
+            <a href="javascript:history.back()">Retourner à la boutique</a> 
+        </div>
+    </section>'
+        <?php
     die('Aucun produit n\'a été ajouter au panier') ; 
 }
 
 
 
-View_Footer::Footer($repere_page_acceuil);
+View_Footer::Footer(@$repere_page_acceuil);
